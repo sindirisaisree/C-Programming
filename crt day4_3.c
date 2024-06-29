d759fa14748b73910efcf1eb0f6b75a2b2b038cb
sindirisaisree <159705134+sindirisaisree@users.noreply.github.com>
2024-06-29 09:28:06 +0530

    Add files via upload

---
+++ crt day4_3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+//int main()
+//{
+//	int x,y;
+////	char s[10]="gfd";
+//	x=printf("hello\n");
+//	printf("x=%d",x);
+////	printf("\ny=%d",y);
+//	return 0;
+//}
+/* hello
+   6(len of string) */
+   
+   
+//void main()
+//{
+//	int a=0;
+//	for(;a;)
+//	{
+//		a++;
+////		printf("%d",a);
+//	}
+//	printf("a=%d",a);
+//}
+//a=0
+
+
+//void main()
+//{
+//	printf("%d",'B'<'A');
+//}//0
+
+
+//void main()
+//{
+//	int i=97;
+//	printf("%c %d",i,i);
+//} //ascii value, number
+
+
+//void main()
+//{
+//	int c=2,d=5,e=10,a;
+//	a=c>1?d>1||e>1?100:200:300;
+//	printf("a=%d",a);
+//}//a=100
