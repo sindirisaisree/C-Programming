4dc03f68db9efe4299c33163ed9e3e06cfe99e1e
sindirisaisree <159705134+sindirisaisree@users.noreply.github.com>
2024-07-02 09:46:34 +0530

    time complexity of for loops

---
+++ crt day6_1.c
@@ -0,0 +1,40 @@
+//time complexity
+1.for(i=0;i<=n;i++){
+	//code
+}//O(n)
+
+2.for(i=0;i<=n;i++){
+	for(i=0;i<=n;i++)
+	{
+		//code
+	}
+}//O(n^2)
+
+3.for(i=0;i<=3*n;i++)
+{
+	//code
+}//O(n)
+
+4.for(i=0;i<=n+5;i++)
+{
+	//code
+}//O(n)
+
+5.for(i=0;i<=n;i+=2)
+{
+	//code
+}//O(n)
+
+6.for(i=0;i<=n;i++){
+	//code
+}//O(n)
+for(i=0;i<=n;i++){
+	for(i=0;i<=n;i++)
+	{
+		//code
+	}
+}//O(n^2)
+for(i=0;i<=n;i++){
+	//code
+}//O(n)
+//total=O(n^2)//max value
